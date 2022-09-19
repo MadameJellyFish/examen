461fcaac0d5d1481ee1ef81ef2f9f031d6fbdc31
Hydre <81431125+HydreNoire@users.noreply.github.com>
2022-09-19 14:12:42 +0200

    ajout des crud admin pour chaque entité

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -2,9 +2,14 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Competence;
+use App\Entity\Examen;
+use App\Entity\Inscription;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -12,16 +17,11 @@ class DashboardController extends AbstractDashboardController
 {
     
     #[Route('/admin', name: 'admin')]
+    // #[IsGranted('ROLE_ADMIN')]
     public function index(): Response
     {
         $url = $this->container->get(AdminUrlGenerator::class);
-        return $this->redirect($url->setController(OneOfYourCrudController::class)->generateUrl());
-
-        // Option 2. You can make your dashboard redirect to different pages depending on the user
-        //
-        // if ('jane' === $this->getUser()->getUsername()) {
-        //     return $this->redirect('...');
-        // }
+        return $this->redirect($url->setController(CompetenceCrudController::class)->generateUrl());
 
         // Option 3. You can render some custom template to display a proper dashboard with widgets, etc.
         // (tip: it's easier if your template extends from @EasyAdmin/page/content.html.twig)
@@ -32,12 +32,15 @@ class DashboardController extends AbstractDashboardController
     public function configureDashboard(): Dashboard
     {
         return Dashboard::new()
-            ->setTitle('Examen');
+            ->setTitle("Centre d'Examen");
     }
 
     public function configureMenuItems(): iterable
     {
-        yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
-        // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
+        yield MenuItem::linkToRoute('Retour sur le site', 'fa-solid fa-arrow-left', 'app_accueil');
+        yield MenuItem::section('Compétences et examens', 'fas fa-list');
+        yield MenuItem::subMenu('Compétences', 'fas fa-list', Competence::class);
+        yield MenuItem::subMenu('Examen', 'fas fa-list', Examen::class);
+        yield MenuItem::subMenu('Inscription', 'fas fa-list', Inscription::class);
     }
 }
