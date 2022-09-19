2f65ee46728a0303b77af5baf6cd62e12ea54350
Hydre <81431125+HydreNoire@users.noreply.github.com>
2022-09-19 12:12:10 +0200

    modification de mot de passe

--- src/Form/EditUtilisateurType.php
+++ src/Form/EditUtilisateurType.php
@@ -3,8 +3,10 @@
 namespace App\Form;
 
 use App\Entity\User;
+use DateTime;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -13,13 +15,16 @@ class EditUtilisateurType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
+        $majorYear = (new DateTime())->format('Y')-18;
+        $lastYear = (new DateTime())->format('Y')-70;
+
         $builder
             ->add('email')
             ->add('nom')
             ->add('prenom')
-            ->add('date', DateTimeType::class, [
+            ->add('date', DateType::class, [
                 'help' => 'Note ici ta date de naissance',
-                'date_format' => 'd M y'
+                'years' => range($lastYear, $majorYear)
             ])
             ->add('telephone')
             ->add('submit', SubmitType::class, [
