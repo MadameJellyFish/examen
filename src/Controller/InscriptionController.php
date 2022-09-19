38a211bba4e24a9f9ff63716e867f2a4cd16ab53
Céline Renault <renault.celine@icloud.com>
2022-09-19 11:54:35 +0200

    modification formulaire inscription competence

--- src/Controller/InscriptionController.php
+++ src/Controller/InscriptionController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Form\InscriptionType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,8 +12,13 @@ class InscriptionController extends AbstractController
     #[Route('/inscription', name: 'app_inscription')]
     public function index(): Response
     {
+        // return $this->render('inscription/index.html.twig', [
+        //     'controller_name' => 'InscriptionController',
+        // ]);
+
+        $form = $this->createForm(InscriptionType::class);
         return $this->render('inscription/index.html.twig', [
-            'controller_name' => 'InscriptionController',
+            'form' => $form->createView()
         ]);
     }
 }
