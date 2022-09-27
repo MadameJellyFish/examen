da1e66656702f103397202695e25c5faf44f6c3f
Lise <lise692@gmail.com>
2022-09-27 10:33:41 +0200

    ajout tostring et maj crud admin

--- src/Controller/Admin/ExamenCrudController.php
+++ src/Controller/Admin/ExamenCrudController.php
@@ -2,7 +2,12 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Competence;
 use App\Entity\Examen;
+use Doctrine\DBAL\Query\QueryBuilder;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
@@ -17,6 +22,42 @@ class ExamenCrudController extends AbstractCrudController
         return Examen::class;
     }
 
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+        ->setPageTitle(Crud::PAGE_INDEX, 'Liste des examens')
+        ->setPageTitle(Crud::PAGE_EDIT, 'Modifier un examen')
+        ->setPageTitle(Crud::PAGE_NEW, 'Créer un examen')
+        ->setPageTitle(Crud::PAGE_DETAIL, 'Consulter un examen');
+    }
+
+    public function configureActions(Actions $actions): Actions{
+        return $actions
+        ->add(Crud::PAGE_INDEX, Action::DETAIL)
+        ->update(Crud::PAGE_INDEX, Action::NEW, function (Action $action) {
+            return $action->setIcon('fa-solid fa-plus pe-1')->setLabel('Ajouter Examen');
+        })
+        ->update(Crud::PAGE_INDEX, Action::DELETE, function (Action $action) {
+            return $action->setIcon('fas fa-trash text-danger')->setLabel('Supprimer Examen');
+        })
+        ->update(Crud::PAGE_INDEX, Action::EDIT, function (Action $action) {
+            return $action->setIcon('fas fa-edit text-warning')->setLabel('Editer Examen');
+        })
+        ->update(Crud::PAGE_INDEX, Action::DETAIL, function (Action $action) {
+            return $action->setIcon('fas fa-eye text-info')->setLabel('Consulter Examen');
+        })
+        ->update(Crud::PAGE_DETAIL, Action::DELETE, function (Action $action) {
+            return $action->setLabel('Supprimer compétence');
+        })
+        ->update(Crud::PAGE_DETAIL, Action::EDIT, function (Action $action) {
+            return $action->setIcon('fas fa-edit text-light')->setLabel('Editer Examen');
+        })
+        ->update(Crud::PAGE_DETAIL, Action::INDEX, function (Action $action) {
+            return $action->setIcon('fa-solid fa-arrow-left text-light')->setLabel('Retour');
+        })
+        ->reorder(Crud::PAGE_DETAIL, [Action::INDEX, Action::EDIT, Action::DELETE]);
+    }
+
     public function configureFields(string $pageName): iterable
     {
         return [
@@ -24,8 +65,7 @@ class ExamenCrudController extends AbstractCrudController
                     ->hideOnDetail()
                     ->hideOnForm()
                     ->hideOnIndex(),
-            AssociationField::new('competence')
-                    ->autocomplete(),
+            AssociationField::new('competence'),
             TextField::new('ville'),
             DateField::new('date'),
             BooleanField::new('valide')
