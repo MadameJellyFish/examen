f75e040e9a3d1d1344353f4d4de78eed86cb4cd7
Ximena Gonzalez <xigonzalez28@gmail.com>
2022-09-28 09:25:47 +0200

    add popup style

--- src/Controller/ProfilController.php
+++ src/Controller/ProfilController.php
@@ -103,6 +103,7 @@ class ProfilController extends AbstractController
     public function delete(EntityManagerInterface $em): Response
     
     {   
+        // dd('ici');
         $user = $this->getUser();
         if(!$user){
             return $this->redirectToRoute('app_login');   
