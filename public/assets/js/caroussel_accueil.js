fd6aee8de3e041d4968d5b8ed497c172a0f548fe
Céline Renault <renault.celine@icloud.com>
2022-09-28 12:03:32 +0200

    add css baniere + test caroussel accueil competence

---
+++ public/assets/js/caroussel_accueil.js
@@ -0,0 +1,25 @@
+// console.log('helooooo');
+// alert('yooyoyoyoooyo');
+
+// let btnL = document.querySelector('#btnLeft');
+// let btnR = document.querySelector('#btnRight');
+// let boxComp = document.querySelector('#boite');
+
+// btnL = 'tooozk,kxdmdc';
+
+// btnL.addEventListener('click', slideL);
+// console.log(btnL);
+
+// function slideL(){
+    //     let imageActive = document.querySelector('.active');
+
+//     imageActive.classList.remove('active');
+  
+//     if (imageActive.previousElementSibling) {
+//         imageActive.previousElementSibling.classList.add('active');
+//     } else {
+//         imageActive.parentElement.lastElementChild.classList.add('active');
+       
+//     }
+
+// }
