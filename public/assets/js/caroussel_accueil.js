5dd790edf20f50a638d30054d909abf27d889a6f
Céline Renault <renault.celine@icloud.com>
2022-09-29 10:08:22 +0200

    modification java script scroll competence accueil

--- public/assets/js/caroussel_accueil.js
+++ public/assets/js/caroussel_accueil.js
@@ -5,16 +5,15 @@ let mainContainer = document.querySelector('#main-box');
 let box = document.querySelector('#boite-comp');
 
 //evenement wheel a 4 propriete dont deltaY (propriete de l'objet évènement) et obtenu par le parametre evt
-mainContainer.addEventListener("wheel", slide)
-function slide(evt){
-    event.preventDefault();
-    mainContainer.scrollLeft +=evt.deltaY;
-    // scrollLeft pour faire un scroll horizontal, c'est une prpriéte du mainContainer
-}
-
-// mainContainer.addEventListener("wheel", (evt) => {
+// mainContainer.addEventListener("wheel", slide)
+// function slide(evt){
 //     evt.preventDefault();
-//     mainContainer.scrollLeft += evt.deltaY;
-// mainContainer.scrollLeft = mainContainer.scrollLeft + event.deltaY;
-// });
+//     mainContainer.scrollLeft +=evt.deltaY;
+//     // scrollLeft pour faire un scroll horizontal, c'est une prpriéte du mainContainer
+// }
+
+mainContainer.addEventListener("wheel", (evt) => {
+    evt.preventDefault();
+    mainContainer.scrollLeft += evt.deltaY;
+});
 
